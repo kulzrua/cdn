7d4c36ab0062c3d6162788372312ae8dfe59fed5
kulzrua <vungtausites@gmail.com>
2019-01-13 10:40:30 +0700

    Add files via upload

---
+++ pvd-goc.js
@@ -0,0 +1,90 @@
+/* https://www.danstools.com/javascript-obfuscate/index.php */
+
+var radomsite = ["https://play.xemphim2015.com/","https://play.haynhat.top/" , "https://video02-02.blogspot.com/?q="];
+var radomsite0 = ["https://play.xemphim2015.com/","https://play.haynhat.top/" , "https://video02-02.blogspot.com/?q="];
+
+var radomsite1 = ["https://video01-01.blogspot.com/?q=","https://video01-02.blogspot.com/?q=", "https://video01-03.blogspot.com/?q=", "https://video01-04.blogspot.com/?q=", "https://video01-05.blogspot.com/?q=" ];
+
+var radomsite2 = ["https://video02-01.blogspot.com/?q=","https://video02-02.blogspot.com/?q=", "https://video02-03.blogspot.com/?q=",  "https://video02-04.blogspot.com/?q=", "https://video02-05.blogspot.com/?q=" ];
+
+var radomsite3 = ["https://video03-01.blogspot.com/?q=","https://video03-02.blogspot.com/?q=", "https://video03-03.blogspot.com/?q=" , "https://video03-04.blogspot.com/?q=", "https://video03-05.blogspot.com/?q=" ];
+
+var radomsite4 = ["https://video04-01.blogspot.com/?q=","https://video04-02.blogspot.com/?q=", "https://video04-03.blogspot.com/?q=" ,  "https://video04-04.blogspot.com/?q=", "https://video04-05.blogspot.com/?q=" ];
+
+var radomsite5 = ["https://video05-01.blogspot.com/?q=","https://video05-02.blogspot.com/?q=", "https://video05-03.blogspot.com/?q=" , "https://video05-04.blogspot.com/?q=", "https://video05-05.blogspot.com/?q=" ];
+
+
+var radomsite6 = ["https://video01-01.blogspot.com/?q=","https://video01-02.blogspot.com/?q=", "https://video01-03.blogspot.com/?q=", "https://video01-04.blogspot.com/?q=", "https://video01-05.blogspot.com/?q=" ];
+
+var radomsite7 = ["https://video02-01.blogspot.com/?q=","https://video02-02.blogspot.com/?q=", "https://video02-03.blogspot.com/?q=",  "https://video02-04.blogspot.com/?q=", "https://video02-05.blogspot.com/?q=" ];
+
+var radomsite8 = ["https://video03-01.blogspot.com/?q=","https://video03-02.blogspot.com/?q=", "https://video03-03.blogspot.com/?q=" , "https://video03-04.blogspot.com/?q=", "https://video03-05.blogspot.com/?q=" ];
+
+var radomsite9 = ["https://video04-01.blogspot.com/?q=","https://video04-02.blogspot.com/?q=", "https://video04-03.blogspot.com/?q=" ,  "https://video04-04.blogspot.com/?q=", "https://video04-05.blogspot.com/?q=" ];
+
+var radomsite10 = ["https://video05-01.blogspot.com/?q=","https://video05-02.blogspot.com/?q=", "https://video05-03.blogspot.com/?q=" , "https://video05-04.blogspot.com/?q=", "https://video05-05.blogspot.com/?q=" ];
+
+var radomsite11 = ["https://video01-01.blogspot.com/?q=","https://video01-02.blogspot.com/?q=", "https://video01-03.blogspot.com/?q=", "https://video01-04.blogspot.com/?q=", "https://video01-05.blogspot.com/?q=" ];
+
+var radomsite12 = ["https://video02-01.blogspot.com/?q=","https://video02-02.blogspot.com/?q=", "https://video02-03.blogspot.com/?q=",  "https://video02-04.blogspot.com/?q=", "https://video02-05.blogspot.com/?q=" ];
+
+var radomsite13 = ["https://video03-01.blogspot.com/?q=","https://video03-02.blogspot.com/?q=", "https://video03-03.blogspot.com/?q=" , "https://video03-04.blogspot.com/?q=", "https://video03-05.blogspot.com/?q=" ];
+
+var radomsite14 = ["https://video04-01.blogspot.com/?q=","https://video04-02.blogspot.com/?q=", "https://video04-03.blogspot.com/?q=" ,  "https://video04-04.blogspot.com/?q=", "https://video04-05.blogspot.com/?q=" ];
+
+var radomsite15 = ["https://video05-01.blogspot.com/?q=","https://video05-02.blogspot.com/?q=", "https://video05-03.blogspot.com/?q=" , "https://video05-04.blogspot.com/?q=", "https://video05-05.blogspot.com/?q=" ];
+
+var radomsite16 = ["https://video01-01.blogspot.com/?q=","https://video01-02.blogspot.com/?q=", "https://video01-03.blogspot.com/?q=", "https://video01-04.blogspot.com/?q=", "https://video01-05.blogspot.com/?q=" ];
+
+var radomsite17 = ["https://video02-01.blogspot.com/?q=","https://video02-02.blogspot.com/?q=", "https://video02-03.blogspot.com/?q=",  "https://video02-04.blogspot.com/?q=", "https://video02-05.blogspot.com/?q=" ];
+
+var radomsite18 = ["https://video03-01.blogspot.com/?q=","https://video03-02.blogspot.com/?q=", "https://video03-03.blogspot.com/?q=" , "https://video03-04.blogspot.com/?q=", "https://video03-05.blogspot.com/?q=" ];
+
+var radomsite4 = ["https://video04-01.blogspot.com/?q=","https://video04-02.blogspot.com/?q=", "https://video04-03.blogspot.com/?q=" ,  "https://video04-04.blogspot.com/?q=", "https://video04-05.blogspot.com/?q=" ];
+
+var radomsite19 = ["https://video05-01.blogspot.com/?q=","https://video05-02.blogspot.com/?q=", "https://video05-03.blogspot.com/?q=" , "https://video05-04.blogspot.com/?q=", "https://video05-05.blogspot.com/?q=" ];
+
+var radomsite20 = ["https://video01-01.blogspot.com/?q=","https://video01-02.blogspot.com/?q=", "https://video01-03.blogspot.com/?q=", "https://video01-04.blogspot.com/?q=", "https://video01-05.blogspot.com/?q=" ];
+
+var radomsite21 = ["https://video02-01.blogspot.com/?q=","https://video02-02.blogspot.com/?q=", "https://video02-03.blogspot.com/?q=",  "https://video02-04.blogspot.com/?q=", "https://video02-05.blogspot.com/?q=" ];
+
+var radomsite22 = ["https://video03-01.blogspot.com/?q=","https://video03-02.blogspot.com/?q=", "https://video03-03.blogspot.com/?q=" , "https://video03-04.blogspot.com/?q=", "https://video03-05.blogspot.com/?q=" ];
+
+var radomsite23 = ["https://video04-01.blogspot.com/?q=","https://video04-02.blogspot.com/?q=", "https://video04-03.blogspot.com/?q=" ,  "https://video04-04.blogspot.com/?q=", "https://video04-05.blogspot.com/?q=" ];
+
+var radomsite24 = ["https://video05-01.blogspot.com/?q=","https://video05-02.blogspot.com/?q=", "https://video05-03.blogspot.com/?q=" , "https://video05-04.blogspot.com/?q=", "https://video05-05.blogspot.com/?q=" ];
+
+var TodayDate = new Date();var h = TodayDate.getHours(); 
+switch (h)
+{
+	case 0 : {radomsite = radomsite0;break;}
+	case 1 : {radomsite = radomsite1;break;}
+	case 2 : {radomsite = radomsite2;break;}
+	case 3 : {radomsite = radomsite3;break;}
+	case 4 : {radomsite = radomsite4;break;}
+	case 5 : {radomsite = radomsite5;break;}
+	case 6 : {radomsite = radomsite6;break;}
+	case 7 : {radomsite = radomsite7;break;}
+	case 8 : {radomsite = radomsite8;break;}
+	case 9 : {radomsite = radomsite9;break;}
+	case 10 : {radomsite = radomsite10;break;}
+	case 11 : {radomsite = radomsite11;break;}
+	case 12 : {radomsite = radomsite12;break;}
+	case 13: {radomsite= radomsite13;break;}
+	case 14: {radomsite= radomsite14; break;}
+	case 15 : {radomsite = radomsite15;break;}
+	case 16: {radomsite= radomsite16;break;}
+	case 17: {radomsite= radomsite17; break;}
+	case 18 : {radomsite = radomsite18;break;}
+	case 19: {radomsite= radomsite19;break;}
+	case 20: {radomsite= radomsite20; break;}
+	case 21 : {radomsite = radomsite21;break;}
+	case 22: {radomsite= radomsite22;break;}
+	case 23: {radomsite= radomsite23; break;}
+	case 24: {radomsite= radomsite24;break;}	
+	default : {console.log("Test: sai");}
+}
+console.log(radomsite.length);
+function getRadomsite() {return radomsite[Math.floor(Math.random() * radomsite.length)];}		
+
+
